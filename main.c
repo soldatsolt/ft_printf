8fc1b845f77d2e10298c51fc254b6acf86db650c
Kiana Mills <kmills@vo-j6.21-school.ru>
2019-04-29 04:53:53 +0300

    Есть очень очень очень кривая реалицазия %s

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: kmills <kmills@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/28 07:26:28 by kmills            #+#    #+#             */
-/*   Updated: 2019/04/29 03:33:42 by kmills           ###   ########.fr       */
+/*   Updated: 2019/04/29 04:53:16 by kmills           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,22 +17,41 @@ int	ft_printf(const char *restrict format, ...)
 	va_list	vl;
 	char	*str;
 	int		i;
+	int		ib;
+	char	*buf;
 
 	i = 0;
+	ib = 0;
+	buf = (char *)malloc(sizeof(char) * 500);
 	va_start(vl, format);
-	str = va_arg(vl, char *);
 	while (format[i])
 	{
-		ft_putchar(format[i]);
-		ft_putchar('\n');
-		i++;
+		if (i > 0 && format[i] == '%' && format[i + 1] == 's')
+		{
+			str = va_arg(vl, char *);
+			i += 2;
+			while (*str)
+			{
+				buf[ib] = *str;
+				str++;
+				ib++;
+			}
+		}
+		if (format[i])
+		{
+			buf[ib] = format[i];
+			i++;
+			ib++;
+		}
 	}
+	buf[ib] = '\0';
+	ft_putstr(buf);
 	va_end(vl);
 	return (i);
 }
 
 int	main(int argc, char **argv)
 {
-	printf("The number of c's in string is: %i\n", ft_printf("123456789"));
+	printf("\nThe number of c's in string is: %i\n", ft_printf("12345%s6789", "12345"));
 	return (0);
 }
