695f860132ff381c48aedcef1f8618f045081e73
Kiana Mills <kmills@vo-a1.21-school.ru>
2019-05-21 06:59:19 +0300

    разбил по функциям main более-менее

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: kmills <kmills@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/28 07:26:28 by kmills            #+#    #+#             */
-/*   Updated: 2019/05/21 03:09:17 by kmills           ###   ########.fr       */
+/*   Updated: 2019/05/21 06:58:48 by kmills           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -236,67 +236,59 @@ int		ft_printf(const char *restrict format, ...)
 	return (ib);
 }
 
-int		main(int argc, char **argv)
+void	make_double_bits_str(unsigned char **s, double d)
 {
-	double			d;
-	unsigned char	*s;
-	unsigned char	*c;
-	int				ic;
 	int				i;
-	double			n;
-	unsigned char	*exp;
-	unsigned char	*mantissa;
-	int				step_exp;
-	double			d_step_exp;
-	double			step_man;
-	unsigned char	*result_double;
+	int				ic;
+	unsigned char	*c;
 
-	d_step_exp = 1;
-	result_double = (unsigned char *)malloc(sizeof(unsigned char) * 60);
-	mantissa = (unsigned char *)malloc(sizeof(unsigned char) * 53);
-	s = (unsigned char *)malloc(sizeof(unsigned char) * 65);
-	exp = (unsigned char *)malloc(sizeof(unsigned char) * 12);
-	ft_bzero(result_double, 60);
-	ft_bzero(mantissa, 53);
-	ft_bzero(s, 65);
-	ft_bzero(exp, 12);
-	d = 123456789101112131415161718192021222324252627282930.123456789;
+	*s = (unsigned char *)malloc(sizeof(unsigned char) * 65);
 	c = (unsigned char *)&d;
-	i = 0;
+	ft_bzero(*s, 65);
 	ic = 7;
+	i = 0;
 	while (ic + 1)
 	{
 		while (i < 8)
 		{
-			s[8 * (7 - ic) + i] = ((c[ic] & (128 >> i)) ? '1' : '0');
+			(*s)[8 * (7 - ic) + i] = ((c[ic] & (128 >> i)) ? '1' : '0');
 			i++;
 		}
 		ic--;
 		i = 0;
 	}
-	s[64] = '\0';
-	printf("DOUBLE  = %.16lf\n", d);
-	printf("%s\n", s);
+	(*s)[64] = '\0';
+}
+
+void	make_exp_mantissa_bits_str(unsigned char *s, unsigned char **exp, \
+unsigned char **mantissa)
+{
+	int	i;
+	int	ic;
+
 	i = 1;
 	ic = 0;
+	*mantissa = (unsigned char *)malloc(sizeof(unsigned char) * 53);
+	*exp = (unsigned char *)malloc(sizeof(unsigned char) * 12);
+	ft_bzero(*mantissa, 53);
+	ft_bzero(*exp, 12);
 	while (i < 12)
-	{
-		exp[ic] = s[i];
-		i++;
-		ic++;
-	}
-	exp[11] = '\0';
-	printf("EXP = %s\n", exp);
+		(*exp)[ic++] = s[i++];
+	(*exp)[11] = '\0';
 	i = 12;
 	ic = 0;
 	while (i < 64)
-	{
-		mantissa[ic] = s[i];
-		i++;
-		ic++;
-	}
-	mantissa[52] = '\0';
-	printf("MAN = %s\n", mantissa);
+		(*mantissa)[ic++] = s[i++];
+	(*mantissa)[52] = '\0';
+}
+
+void	make_d_exp(unsigned char *exp, double *d_step_exp)
+{
+	int	step_exp;
+	int i;
+	int ic;
+	
+	*(d_step_exp) = 1;
 	i = 10;
 	ic = 0;
 	step_exp = 0;
@@ -307,37 +299,69 @@ int		main(int argc, char **argv)
 		i--;
 	}
 	step_exp -= 1023;
-	printf("VOT ETA = %i\n", step_exp);
 	if (step_exp < 0)
 	{
-		d_step_exp = (double)((double)1 / (double)(1 << (-step_exp)));
-		printf("EXP STE = %.16lf\n", d_step_exp);
+		*(d_step_exp) = (double)((double)1 / (double)(1 << (-step_exp)));
 	}
 	else
 	{
 		i = 0;
-		while (i < step_exp)
-		{
-			d_step_exp *= 2;
-			i++;
-		}
-		printf("EXP STE = %.16lf\n", d_step_exp);
+		while (i++ < step_exp)
+			*(d_step_exp) *= 2;
 	}
-	step_man = 0;
+}
+
+void	make_rez_d(double d_step_exp, unsigned char *mantissa, double *step_man, double *n)
+{
+	int i;
+
+	*step_man = 0;
 	i = 51;
 	while (i + 1)
 	{
 		if (mantissa[i] == '1')
-			step_man += (double)((double)1 / (double)((int64_t)1 << (i + 1)));
+			*step_man += (double)((double)1 / (double)((int64_t)1 << (i + 1)));
 		i--;
 	}
 
-	step_man += 1;
+	*step_man += 1;
 
-	printf("MAN STE = %.16lf\n", step_man);
-	n = d_step_exp * step_man;
+	*n = (d_step_exp) * (*step_man);
+	printf("RES DBL = %.16lf\n", *n);
+}
+
+int		main(int argc, char **argv)
+{
+	double			d;
+	unsigned char	*s;
+	double			n;
+	unsigned char	*exp;
+	unsigned char	*mantissa;
+	double			d_step_exp;
+	double			step_man;
+	unsigned char	*result_double;
+
+	result_double = (unsigned char *)malloc(sizeof(unsigned char) * 100);
+	ft_bzero(result_double, 100);
+	d = 12345.1234;
+	printf("DOUBLE  = %.16lf\n", d);
+
+	
+	make_double_bits_str(&s, d);
+	make_exp_mantissa_bits_str(s, &exp, &mantissa);
+	make_d_exp(exp, &d_step_exp);
 	if (s[0] == '1')
-		n = -n;
-	printf("RES DBL = %.16lf\n", n);
+		d_step_exp = -d_step_exp;
+	make_rez_d(d_step_exp, mantissa, &step_man, &n);
+
+/*
+	//       DEBUG
+	printf("STR = %s\n", s);
+	printf("EXP = %s\n", exp);
+	printf("MAN = %s\n", mantissa);
+	printf("EXP STE = %.16lf\n", d_step_exp);
+	printf("MAN STE = %.16lf\n", step_man);
+*/
+
 	return (0);
 }
