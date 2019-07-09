da11a69e84dc727920adb668e0ac46c118a9f133
Kiana Mills <kmills@oa-b3.21-school.ru>
2019-07-09 04:47:40 +0300

    просто строка работает без лишних флагов тип пробела и тд

--- main.c
+++ main.c
@@ -6,15 +6,83 @@
 /*   By: kmills <kmills@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/28 07:26:28 by kmills            #+#    #+#             */
-/*   Updated: 2019/07/09 04:06:12 by kmills           ###   ########.fr       */
+/*   Updated: 2019/07/09 04:47:10 by kmills           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-void	turbo_parser(va_list vl, t_buf *buf, const char *restrict format)
+t_buf *g_start;
+
+void	s_flag(va_list vl, t_buf **buf, const char *restrict format, t_flags fl)
 {
-	printf("\n\n==PARSER==\n\nformat = %s\n\n==PARSER==\n\n", format);
+	char	*str; // TODO: мб ****** эти лишние
+
+	str = va_arg(vl, char *);
+	*buf = put_str_to_buf(*buf, str);
+}
+
+void	make_t_flags0(t_flags *fl)
+{
+	fl->dash = 0;
+	fl->minus = 0;
+	fl->plus = 0;
+	fl->space = 0;
+	fl->zero = 0;
+	fl->width = 0;
+	fl->precision = 0;
+}
+
+void	make_t_width(t_flags *fl, const char *restrict *format)
+{
+	fl->width = ft_atoi(*format);
+	while ((**format) >= '0' && (**format) <= '9')
+		(*format)++;
+	(*format)--;
+}
+
+void	make_t_precision(t_flags *fl, const char *restrict *format)
+{
+	(*format)++;
+	fl->precision = ft_atoi(*format);
+	while ((**format) >= '0' && (**format) <= '9')
+		(*format)++;
+	(*format)--;
+}
+
+void	preparcing(t_flags *fl, const char *restrict *format)
+{
+	while ((**format) == ' ' || (**format) == '-' || (**format) == '+' || \
+	((**format) >= '0' && (**format) <= '9') || (**format) == '#' || \
+	(**format) == '.')
+	{
+		if ((**format) == ' ')
+			fl->space = 1;
+		if ((**format) == '#')
+			fl->dash = 1;
+		if ((**format) == '-')
+			fl->minus = 1;
+		if ((**format) == '+')
+			fl->plus = 1;
+		if ((**format) == '0')
+			fl->zero = 1;
+		if	((**format) > '0' && (**format) <= '9')
+			make_t_width(fl, format);
+		if ((**format) == '.')
+			make_t_precision(fl, format);
+		(*format)++;
+	}
+}
+
+void	turbo_parser(va_list vl, t_buf **buf, const char *restrict *format)
+{
+	t_flags	fl;
+	
+	make_t_flags0(&fl);
+	preparcing(&fl, format);
+	if (**format == 's')
+		s_flag(vl, buf, *format, fl);
+	(*format)++;
 }
 
 void	ft_printf2(va_list vl, t_buf *buf, const char *restrict format)
@@ -29,7 +97,7 @@ void	ft_printf2(va_list vl, t_buf *buf, const char *restrict format)
 		else if (*format == '%' && format[1] != '%')
 		{
 			format++; // теперь в ф-ии парсера сразу идёт флаг (без %)
-			turbo_parser(vl, buf, format);
+			turbo_parser(vl, &buf, &format);
 		}
 		else
 		{
@@ -48,6 +116,7 @@ int		ft_printf(const char *restrict format, ...)
 
 	buf = create_buf();
 	start = buf;
+	g_start = start;
 	va_start(vl, format);
 	ft_printf2(vl, buf, format);
 	printf_buf(start);
@@ -59,6 +128,6 @@ int		ft_printf(const char *restrict format, ...)
 
 int		main(int argc, char **argv)
 {
-	ft_printf("123542%s%i6rere%%");
+	ft_printf("123542% s%%i6rere%%", "==ETA STROKA==");
 	return (0);
 }
--- old_main_functions.c
+++ old_main_functions.c
@@ -1,133 +1,133 @@
 #include "ft_printf.h"
 
-void	s_flag(char *str, char **buf, int *ib,\
-t_flags fl)
-{
-	int		i;
-	int		l;
+// void	s_flag(char *str, char **buf, int *ib,\
+// t_flags fl)
+// {
+// 	int		i;
+// 	int		l;
 
-	i = 0;
-	l = (int)(ft_strlen(str));
-	if (fl.minus == 0)
-		while (fl.width > l + i)
-		{
-			**buf = ' ';
-			(*buf)++;
-			(*ib)++;
-			i++;
-		}
-	while (*str)
-	{
-		**buf = *str;
-		str++;
-		(*buf)++;
-		(*ib)++;
-	}
-	if (fl.minus != 0)
-	{
-		while (fl.width > l + i)
-		{
-			**buf = ' ';
-			(*buf)++;
-			(*ib)++;
-			i++;
-		}
-	}
-}
+// 	i = 0;
+// 	l = (int)(ft_strlen(str));
+// 	if (fl.minus == 0)
+// 		while (fl.width > l + i)
+// 		{
+// 			**buf = ' ';
+// 			(*buf)++;
+// 			(*ib)++;
+// 			i++;
+// 		}
+// 	while (*str)
+// 	{
+// 		**buf = *str;
+// 		str++;
+// 		(*buf)++;
+// 		(*ib)++;
+// 	}
+// 	if (fl.minus != 0)
+// 	{
+// 		while (fl.width > l + i)
+// 		{
+// 			**buf = ' ';
+// 			(*buf)++;
+// 			(*ib)++;
+// 			i++;
+// 		}
+// 	}
+// }
 
-int		razrydnost(int n)
-{
-	int r;
-	int i;
+// int		razrydnost(int n)
+// {
+// 	int r;
+// 	int i;
 
-	i = 10;
-	r = 1;
-	while (n / i > 0)
-	{
-		i *= 10;
-		r++;
-	}
-	return (r);
-}
+// 	i = 10;
+// 	r = 1;
+// 	while (n / i > 0)
+// 	{
+// 		i *= 10;
+// 		r++;
+// 	}
+// 	return (r);
+// }
 
-void	i_flag(va_list vl, char **buf, int *ib,\
-t_flags fl)
-{
-	int		num;
-	char	*str;
-	int		i;
-	char	*s;
+// void	i_flag(va_list vl, char **buf, int *ib,\
+// t_flags fl)
+// {
+// 	int		num;
+// 	char	*str;
+// 	int		i;
+// 	char	*s;
 
-	i = 0;
-	num = va_arg(vl, int);
-	str = (char *)malloc(sizeof(char) * (razrydnost(num) + 1 + fl.precision + fl.width));
-	s = str;
-	if (fl.zero)
-		while (fl.width - i)
-		{
-			*str = '0';
-			str++;
-			i++;
-		}
-	else
-		while (fl.precision - i)
-		{
-			*str = '0';
-			str++;
-			i++;
-		}
-	*str = '\0';
-	i = 0;
-	str = ft_itoa(num);
-	str[razrydnost(num)] = '\0';
-	if (fl.zero)
-		while (str[i])
-		{
-			s[fl.width - razrydnost(num) + i] = str[i];
-			i++;
-		}
-	else if (fl.precision > razrydnost(num))
-		while (str[i])
-		{
-			s[fl.precision - razrydnost(num) + i] = str[i];
-			i++;
-		}
-	else
-		while (str[i])
-		{
-			s[i] = str[i];
-			i++;
-		}
-	s_flag(s, buf, ib, fl);
-}
+// 	i = 0;
+// 	num = va_arg(vl, int);
+// 	str = (char *)malloc(sizeof(char) * (razrydnost(num) + 1 + fl.precision + fl.width));
+// 	s = str;
+// 	if (fl.zero)
+// 		while (fl.width - i)
+// 		{
+// 			*str = '0';
+// 			str++;
+// 			i++;
+// 		}
+// 	else
+// 		while (fl.precision - i)
+// 		{
+// 			*str = '0';
+// 			str++;
+// 			i++;
+// 		}
+// 	*str = '\0';
+// 	i = 0;
+// 	str = ft_itoa(num);
+// 	str[razrydnost(num)] = '\0';
+// 	if (fl.zero)
+// 		while (str[i])
+// 		{
+// 			s[fl.width - razrydnost(num) + i] = str[i];
+// 			i++;
+// 		}
+// 	else if (fl.precision > razrydnost(num))
+// 		while (str[i])
+// 		{
+// 			s[fl.precision - razrydnost(num) + i] = str[i];
+// 			i++;
+// 		}
+// 	else
+// 		while (str[i])
+// 		{
+// 			s[i] = str[i];
+// 			i++;
+// 		}
+// 	s_flag(s, buf, ib, fl);
+// }
 
-void	make_t_flags(t_flags *fl)
-{
-	fl->dash = 0;
-	fl->minus = 0;
-	fl->plus = 0;
-	fl->space = 0;
-	fl->zero = 0;
-	fl->width = 0;
-	fl->precision = 0;
-}
+// void	make_t_flags(t_flags *fl)
+// {
+// 	fl->dash = 0;
+// 	fl->minus = 0;
+// 	fl->plus = 0;
+// 	fl->space = 0;
+// 	fl->zero = 0;
+// 	fl->width = 0;
+// 	fl->precision = 0;
+// }
 
-void	make_t_width(t_flags *fl, const char *restrict *format)
-{
-	(*fl).width = ft_atoi(*format);
-	while ((**format) >= '0' && (**format) <= '9')
-		(*format)++;
-	(*format)--;
-}
+// void	make_t_width(t_flags *fl, const char *restrict *format)
+// {
+// 	(*fl).width = ft_atoi(*format);
+// 	while ((**format) >= '0' && (**format) <= '9')
+// 		(*format)++;
+// 	(*format)--;
+// }
 
-void	make_t_precision(t_flags *fl, const char *restrict *format)
-{
-	(*format)++;
-	(*fl).precision = ft_atoi(*format);
-	while ((**format) >= '0' && (**format) <= '9')
-		(*format)++;
-	(*format)--;
-}
+// void	make_t_precision(t_flags *fl, const char *restrict *format)
+// {
+// 	(*format)++;
+// 	(*fl).precision = ft_atoi(*format);
+// 	while ((**format) >= '0' && (**format) <= '9')
+// 		(*format)++;
+// 	(*format)--;
+// }
 
 // void	turbo_parser(va_list vl, char **buf, int *ib,\
 // const char *restrict *format)
@@ -136,26 +136,26 @@ void	make_t_precision(t_flags *fl, const char *restrict *format)
 	
 // 	make_t_flags(&fl);
 // 	(*format)++; // теперь указатель не на проценте, а на флаге
-// 	while ((**format) == ' ' || (**format) == '-' || (**format) == '+' || \
-// 	((**format) >= '0' && (**format) <= '9') || (**format) == '#' || \
-// 	(**format) == '.')
-// 	{
-// 		if ((**format) == ' ')
-// 			fl.space = 1;
-// 		if ((**format) == '#')
-// 			fl.dash = 1;
-// 		if ((**format) == '-')
-// 			fl.minus = 1;
-// 		if ((**format) == '+')
-// 			fl.plus = 1;
-// 		if ((**format) == '0')
-// 			fl.zero = 1;
-// 		if	((**format) > '0' && (**format) <= '9')
-// 			make_t_width(&fl, format);
-// 		if ((**format) == '.')
-// 			make_t_precision(&fl, format);
-// 		(*format)++;
-// 	}
+	// while ((**format) == ' ' || (**format) == '-' || (**format) == '+' || \
+	// ((**format) >= '0' && (**format) <= '9') || (**format) == '#' || \
+	// (**format) == '.')
+	// {
+	// 	if ((**format) == ' ')
+	// 		fl.space = 1;
+	// 	if ((**format) == '#')
+	// 		fl.dash = 1;
+	// 	if ((**format) == '-')
+	// 		fl.minus = 1;
+	// 	if ((**format) == '+')
+	// 		fl.plus = 1;
+	// 	if ((**format) == '0')
+	// 		fl.zero = 1;
+	// 	if	((**format) > '0' && (**format) <= '9')
+	// 		make_t_width(&fl, format);
+	// 	if ((**format) == '.')
+	// 		make_t_precision(&fl, format);
+	// 	(*format)++;
+	// }
 // 	if ((**format) == 's')
 // 	{
 // 		s_flag(va_arg(vl, char *), buf, ib, fl);
