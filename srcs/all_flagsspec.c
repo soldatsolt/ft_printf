ba9f72d581e3d17d24c5a676874e7e68cb1e5947
Kiana Mills <kmills@oa-a2.21-school.ru>
2019-08-01 06:11:23 +0300

    all_flagsspec.c almost normed almost is AALMOST DONE REALLY

--- srcs/all_flagsspec.c
+++ srcs/all_flagsspec.c
@@ -4,8 +4,8 @@ void	s_flag(char *str, t_buf **buf, t_flags fl)
 {
 	int		n;
 
-    if (fl.precision != -1)
-        fl.zero = 0;
+	if (fl.precision != -1)
+		fl.zero = 0;
 	if (str)
 	{
 		str = ft_strdup(str);
@@ -25,7 +25,7 @@ void	s_flag(char *str, t_buf **buf, t_flags fl)
 		free(str);
 	}
 	else
-		s_flag("(null)", buf, fl); // тут тоже фришить нужно
+		s_flag("(null)", buf, fl);
 }
 
 void	p_flag(va_list vl, t_buf **buf, t_flags fl)
@@ -46,6 +46,57 @@ void	p_flag(va_list vl, t_buf **buf, t_flags fl)
 	free(s);
 }
 
+void	i_flag1(t_buf **buf, t_flags fl, char *str, int k)
+{
+	int		n;
+	char	z;
+
+	z = (k >= 0) ? '+' : '-';
+	n = fl.width - (int)ft_strlen(str);
+	if (fl.plus && k >= 0)
+		n--;
+	if (n > 0 && !fl.minus)
+	{
+		put_char_to_buf(buf, z);
+		if (fl.zero)
+			put_some_chars_to_buf(buf, '0', n);
+		else
+			put_some_chars_to_buf(buf, ' ', n);
+	}
+	if (k < 0 && (int)ft_strlen(str) < fl.width)
+		put_str_to_buf(buf, str + 1);
+	else
+		put_str_to_buf(buf, str);
+	if (n > 0 && fl.minus)
+		put_some_chars_to_buf(buf, ' ', n);
+}
+
+void	i_flag2(t_buf **buf, t_flags fl, char *str, int k)
+{
+	int		n;
+	char	z;
+
+	z = (k >= 0) ? '+' : '-';
+	n = fl.width - (int)ft_strlen(str);
+	if (fl.space && k >= 0)
+	{
+		put_char_to_buf(buf, ' ');
+		n--;
+	}
+	if (n > 0 && !fl.minus)
+	{
+		if (fl.zero)
+			put_some_chars_to_buf(buf, '0', n);
+		else
+			put_some_chars_to_buf(buf, ' ', n);
+	}
+	if (fl.plus && k >= 0)
+		put_char_to_buf(buf, z);
+	put_str_to_buf(buf, str);
+	if (n > 0 && fl.minus)
+		put_some_chars_to_buf(buf, ' ', n);
+}
+
 void	i_flag(va_list vl, t_buf **buf, t_flags fl)
 {
 	char	*str;
@@ -53,8 +104,8 @@ void	i_flag(va_list vl, t_buf **buf, t_flags fl)
 	int		k;
 	char	z;
 
-    if (fl.precision != -1)
-        fl.zero = 0;
+	if (fl.precision != -1)
+		fl.zero = 0;
 	k = va_arg(vl, int);
 	if (k == (int)0 && fl.precision == 0)
 		return ;
@@ -63,50 +114,17 @@ void	i_flag(va_list vl, t_buf **buf, t_flags fl)
 	n = fl.width - (int)ft_strlen(str);
 	if (fl.plus && k >= 0)
 		n--;
-    if ((k < 0 || fl.plus) && fl.zero)
-    {
-        if (n > 0 && !fl.minus)
-        {
-            put_char_to_buf(buf, z);
-            if (fl.zero)
-                put_some_chars_to_buf(buf, '0', n);
-            else
-                put_some_chars_to_buf(buf, ' ', n);
-        }
-		if (k < 0 && (int)ft_strlen(str) < fl.width)
-       		put_str_to_buf(buf, str + 1); // из-за +1 при отриц числе и флаге 0
-		else						// не выводится '-' TODO: mb mistake in if
-			put_str_to_buf(buf, str);		
-        if (n > 0 && fl.minus)
-            put_some_chars_to_buf(buf, ' ', n);
-    }
-    else
-    {
-		if (fl.space && k >= 0)
-		{
-        	put_char_to_buf(buf, ' ');
-			n--;
-		}
-        if (n > 0 && !fl.minus)
-        {
-            if (fl.zero)
-                put_some_chars_to_buf(buf, '0', n);
-            else
-                put_some_chars_to_buf(buf, ' ', n);
-        }
-        if (fl.plus && k >= 0)
-            put_char_to_buf(buf, z);
-        put_str_to_buf(buf, str);
-        if (n > 0 && fl.minus)
-            put_some_chars_to_buf(buf, ' ', n);
-    }
+	if ((k < 0 || fl.plus) && fl.zero)
+		i_flag1(buf, fl, str, k);
+	else
+		i_flag2(buf, fl, str, k);
 	free(str);
 }
 
 void	c_flag(va_list vl, t_buf **buf, t_flags fl)
 {
 	char	c;
-	
+
 	if (!fl.minus && fl.width > 1)
 	{
 		if (fl.zero)
@@ -120,6 +138,22 @@ void	c_flag(va_list vl, t_buf **buf, t_flags fl)
 		put_some_chars_to_buf(buf, ' ', fl.width - 1);
 }
 
+void	o_flag1(t_flags fl, t_buf **buf, char *s, char *str)
+{
+	s = ft_catstr("0", str);
+	if (fl.precision != -1)
+	{
+		fl.zero = 0;
+		fl.precision = -1;
+	}
+	free(str);
+	str = NULL;
+	str = s;
+	s_flag(s, buf, fl);
+	free(str);
+	return ;
+}
+
 void	o_flag(va_list vl, t_buf **buf, t_flags fl)
 {
 	char			*str;
@@ -127,27 +161,14 @@ void	o_flag(va_list vl, t_buf **buf, t_flags fl)
 	unsigned int	k;
 	char			*s;
 
-    if (fl.precision != -1)
-        fl.zero = 0;
+	if (fl.precision != -1)
+		fl.zero = 0;
 	k = va_arg(vl, unsigned int);
 	if (k == (unsigned int)0 && fl.precision == 0)
 		return ;
 	str = make_str_with_precision_for_o(fl, k);
 	if (fl.dash && k != 0 && str[0] != '0')
-	{
-		s = ft_catstr("0", str);
-		if (fl.precision != -1)
-		{
-			fl.zero = 0;
-			fl.precision = -1;
-		}
-		free(str);
-		str = NULL;
-		str = s;
-		s_flag(s, buf, fl);
-		free(str);
-		return ;
-	}
+		return (o_flag1(fl, buf, s, str));
 	n = fl.width - (int)ft_strlen(str);
 	if (n > 0 && !fl.minus)
 	{
@@ -162,33 +183,40 @@ void	o_flag(va_list vl, t_buf **buf, t_flags fl)
 	free(str);
 }
 
-void	x_flag(va_list vl, t_buf **buf, t_flags fl, char *(*f)(unsigned int, int))
+void	x_flag1(t_buf **buf, t_flags fl, char *(*f)(unsigned int, int), \
+unsigned int k)
+{
+	char	*str;
+	char	*s;
+	char	*ox;
+
+	str = f(k, 16);
+	if (f == &ft_itoa_base_small)
+		ox = make_ox_for_x(ox, &fl, (int)ft_strlen(str));
+	else
+		ox = make_ox_for_X(ox, &fl, (int)ft_strlen(str));
+	s = ft_catstr(ox, str);
+	s_flag(s, buf, fl);
+	free(str);
+	free(s);
+	free(ox);
+	return ;
+}
+
+void	x_flag(va_list vl, t_buf **buf, t_flags fl, char *(*f)\
+(unsigned int, int))
 {
 	char			*str;
 	int				n;
 	unsigned int	k;
-	char			*ox;
-	char			*s;
 
-    if (fl.precision != -1)
-        fl.zero = 0;
+	if (fl.precision != -1)
+		fl.zero = 0;
 	k = va_arg(vl, unsigned int);
 	if (k == (unsigned int)0 && fl.precision == 0)
 		return ;
-	if (fl.dash && k!= 0)
-	{
-		str = f(k ,16);
-		if (f == &ft_itoa_base_small)
-			ox = make_ox_for_x(ox, &fl, (int)ft_strlen(str));
-		else
-			ox = make_ox_for_X(ox, &fl, (int)ft_strlen(str));
-		s = ft_catstr(ox, str);
-		s_flag(s, buf, fl);
-		free(str);
-		free(s);
-		free(ox);
-		return ;
-	}
+	if (fl.dash && k != 0)
+		return (x_flag1(buf, fl, f, k));
 	str = make_str_with_precision_for_x(fl, k, f);
 	n = fl.width - (int)ft_strlen(str);
 	if (n > 0 && !fl.minus)
@@ -210,8 +238,8 @@ void	u_flag(va_list vl, t_buf **buf, t_flags fl)
 	int				n;
 	unsigned int	k;
 
-    if (fl.precision != -1)
-        fl.zero = 0;
+	if (fl.precision != -1)
+		fl.zero = 0;
 	k = va_arg(vl, unsigned int);
 	if (k == (unsigned int)0 && fl.precision == 0)
 		return ;
